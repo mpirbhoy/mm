f8cff2097b975bb1aa8ddeb02f86e8c6f85d4845
mpirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2015-12-29 00:25:56 -0500

    made changes

--- public/js/cal.js
+++ public/js/cal.js
@@ -1,12 +1,12 @@
-var timings1 = {days: [{start: 10, end: 11.25, day: "M"}], course: "CSC209", section: "Section A", type: "LECT"};
+var timings1 = {days: [{start: 10.75, end: 11.25, day: "F"}], course: "CSC209", section: "Section A", type: "LECT"};
 
 function click1() {
 	var fallDiv = document.getElementById("fall-cal");
-	var allRows = fallDiv.childNodes[1].childNodes;
+	var allRows = fallDiv.getElementsByTagName('tr');
 	for (var i = 0; i < timings1.days.length; i++) {
-		var startIndex = (timings1.days[i].start - 8) * 4;
-		var endIndex = (timings1.days[i].end - 8) * 4;
-		var diff = endIndex - startIndex;
+		var startIndex = (timings1.days[i].start - 8) * 4 + 1;
+		var endIndex = (timings1.days[i].end - 8) * 4 + 1;
+		var diff = endIndex - startIndex + 1;
 		var dayIndex;
 		switch(timings1.days[i].day) {
 			case 'M':
@@ -25,12 +25,23 @@ function click1() {
 				dayIndex = 5;
 				break;
 		}
+		var posJSON = [];
 		for (var ii = 0; ii < diff; ii++) {
-			console.log(startIndex);
-			console.log(allRows);
-			console.log(allRows.childNodes);
-			allRows.childNodes[startIndex].childNodes[dayIndex].setAttribute("class", "selected");
+			console.log("startIndex: " + startIndex + ii);
+			console.log("dayIndex: " + dayIndex + 1);
+			var currCell = allRows[startIndex + ii].getElementsByTagName("td");
+			currCell[dayIndex].setAttribute("class", "selected");
+
 		}
+/*
+		//Create Div for overlay here
+		var newDiv = document.createElement("div");
+		newDiv.setAttribute("class", "overlayDiv");
+		var newText = document.createTextNode("Hello World");
+		newDiv.appendChild(newText);
+		var fallOverlayDiv = document.getElementById("fall-overlay");
+		//fallOverlayDiv.appendChild(newDiv);
+		*/
 	}
 }
 
