b8625c6e23c71bd51a273eb1167c04901e9c2c8b
mpirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2015-12-29 16:34:20 -0500

    added term for section

--- model/section.js
+++ model/section.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 //Model for University Courses 
 module.exports = mongoose.model('Course',{
     sectionCode: {type: String, required:true},//sectioncode for Section
+    term: {type:String, required:true},//term for Section
     sectionDirector: {type: String, required: true},//Director for Section
     instructors: String, //instructors for the Section
     catalogs: [{type: mongoose.Schema.Types.ObjectId, ref: 'Catalog' }], //Catalogs belonging to Section
