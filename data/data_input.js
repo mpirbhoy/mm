c12b0822362cca3efa47755fc736c4a1c4feb6c9
mpirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2015-12-29 17:45:17 -0500

    moved to data directory

--- data/data_input.js
+++ data/data_input.js
@@ -16,7 +16,74 @@ function readFile(file) {
     };
     rawFile.send(null);
 }
-module.exports = function (){
+
+function getTerm(rawStr) {
+    var rawSplit = rawStr.split(' ');
+    return rawSplit[1];
+}
+
+function getSectionCode(rawStr) {
+    var rawSplit = rawStr.split(' ');
+    return rawSplit[3];
+}
+
+function getSectionDirector(rawStr) {
+    var rawSplit = rawStr.split(':');
+    var returnVal = rawSplit[1];
+    return returnVal.substring(1);
+}
+
+
+function getDayStarttimeDuration(timingInput) {
+    //"M--12:00--120--YH  045      (Glendon campus)--||T--12:00--120--YH  B211     (Glendon campus)--||"
+    var meeting, res, temp, meetingInfo;
+    var daysEnum = {
+        M: "Monday",
+        T: "Tuesday",
+        W: "Wednesday",
+        R: "Thursday",
+        F: "Friday"
+    }
+    var meetings = timingInput.split("||");
+    // ["M--12:00--120--YH  045      (Glendon campus)--", "T--12:00--120--YH  B211     (Glendon campus)--", ""]
+    res = [];
+    for (var i = 0; i < meetings.length; i++) {
+
+        if (meetings[i]) {
+
+            temp = {};
+
+            meetingInfo = meetings[i].split("--");
+            //["M", "12:00", "120", "YH  045      (Glendon campus)", ""]
+
+            temp = {
+                day: daysEnum[meetingInfo[0]],
+                startTime: meetingInfo[1],
+                duration: meetingInfo[2],
+                location: meetingInfo[3]
+            };
+
+            res.push(temp);
+        }
+    }
+    return res[0];
+}
+
+function getCourseTitleInfo(inputString){
+    
+    var FacCode  = inputString.split("/")[0];
+    var courseCodeAndTitle = inputString.split("/")[1]; // RYER 4000   3.00 Ryerson York Exchange Course
+
+    var arrayOfcourseCodeAndTitle = courseCodeAndTitle.split("   "); //[RYER 4000, 3.00 Ryerson York Exchange Course]
+
+    var resCourseCode = arrayOfcourseCodeAndTitle[0].replace(' ', '');
+    var resCredit = arrayOfcourseCodeAndTitle[1].split(' ')[0];
+    var resCourseTitle = arrayOfcourseCodeAndTitle[1].replace(resCredit, '');
+
+    return {courseCode: resCourseCode.trim(), credit: resCredit.trim(), courseName: resCourseTitle.trim()};
+
+}
+module.exports = function () {
     var row, field;
     var dataFields = {
         FACCODE: 0,
@@ -34,14 +101,210 @@ module.exports = function (){
     };
     var dataLoc = 'file:///Users/franklai/code/mm/data/york_data0.json';
     var allData = readFile(dataLoc);
+    
+    //
+    for (var i = 0; i < allData.rows.length; i++){
+        
+        // Conversions
+        var arrayCourseTitleInfo = getCourseTitleInfo(allData.rows[i][dataFields.COURSE_TITLE]);
+        var varyingMeetingsInfo = getDayStarttimeDuration(allData.rows[i][dataFields.VARY_MEETING_DAY_START_TIME_DURATION]);
+        
+        
+        //Courses
+        var courseCode = arrayCourseTitleInfo.courseCode; //String
+        var facultyCode =  allData.rows[i][dataFields.FACCODE].replace(" -", ""); //String
+        var courseName = arrayCourseTitleInfo.courseName;  //String
+        
+        var indexOfFirstPrereq = allData.rows[i][dataFields.COURSE_DESC].indexOf("Prereq");
+        var indexofFirstCourseExclusion = allData.rows[i][dataFields.COURSE_DESC].indexOf("Course credit exclusion");
+        
+        var prereqs = allData.rows[i][dataFields.COURSE_DESC].substring(indexOfFirstPrereq, indexofFirstCourseExclusion); //[String]
+        var exclusions = allData.rows[i][dataFields.COURSE_DESC].substring(indexofFirstCourseExclusion); //[String]
+        var courseNote = allData.rows[i][dataFields.COURSE_DESC].substring(0, indexOfFirstPrereq); //String
 
-    //for (row in allData.rows) {
-    //   for (field in dataFields) {
-    //        new Catalog(
-    //            {
-    //            }
-    //        )
-    //   }
-    //}
+        //Sections
+        var sectionCode = getSectionCode(allData.rows[i][dataFields.TERM_AND_SECTION]);
+        var term = getTerm(allData.rows[i][dataFields.TERM_AND_SECTION]);
+        var sectionDirector = getSectionDirector(allData.rows[i][dataFields.SECTION_DIRECTOR]);
+        var sectionInstructors = allData.rows[i].dataFields.REQ_MEETING_INSTRUCTOR;
+        var sectionMeeting = getDayStarttimeDuration(allData.rows[i][dataFields.REQ_MEETING_DAY_START_TIME_DURATION]);
+        
+        //Catalogs
+        var catalogCode = allData.rows[i].dataFields.CAT_NUM;
+        var catalogInstructors = allData.rows[i].dataFields.VARY_MEETING_INSTRUCTOR;
+        var catalogMeeting = getDayStarttimeDuration(allData.rows[i][dataFields.VARY_MEETING_DAY_START_TIME_DURATION]);
+        
+        //If current row is not a catalog
+        if (!allData[i][dataFields.CAT_NUM]) {
+            //Search for course
+            Course.where({courseCode: courseCode}).findOne().populate('sections').lean().exec(function (err, myCourse) {
+                //If course found
+                if (myCourse) {
+                    var sectionExists = false;
+                    var sectionIndex = null;
+                    
+                    // Populating catalog
+                    Section.populate(myCourse['sections'], {path: 'catalogs'}, function (err, data) {
+                        
+                        for (var ii = 0; ii < data.sections.length; ii++) {
+                            if (data.sections[i].sectionCode == sectionCode) {
+                                sectionExists = true;
+                                sectionIndex = ii;
+                            }
+                        }
+                        
+                        if (!sectionExists) {
+                            //CREATE SECTION 
+                            var newSection = new Section({
+                                sectionCode: sectionCode,
+                                term: term,
+                                sectionDirector: sectionDirector,
+                                instructors: sectionInstructors,
+                                catalogs: [],
+                                sectionMeetings: [sectionMeeting]
+                            });
+                            newSection.save();
+                            
+                            //ADD SECTION TO COURSE
+                            data.sections.push(newSection);
+                            //SAVE
+                            data.save();
+                        
+                        } else {
+                            //UPDATE SECTIONS
+                            data.sections[sectionIndex].sectionMeetings.push(sectionMeeting);
+                            //SAVE
+                            data.save();
+                        }
+                            
+                        
+                    });
+                
+                //If course is not found    
+                } else {
+                    //CREATE SECTION
+                    var newSection = new Section({
+                        sectionCode: sectionCode,
+                        term: term,
+                        sectionDirector: sectionDirector,
+                        instructors: sectionInstructors,
+                        catalogs: [],
+                        sectionMeetings: [sectionMeeting]
+                    });
+                    newSection.save();
+                    
+                    //CREATE COURSE AND ADD SECTION TO COURSE
+                    var newCourse = new Course({
+                        courseCode: courseCode,
+                        facultyCode: facultyCode,
+                        prereqs: prereqs,
+                        exclusions: exclusions,
+                        sections: [newSection],
+                        note: courseNote
+                    });
+                    newCourse.save();
+                    
+                }
+            });
+            
+            
+        
+        //If current row is a Catalog
+        } else {
+            //Search for course
+            Course.where({courseCode: courseCode}).findOne().populate('sections').lean().exec(function (err, myCourse) {
+                //If course found
+                if (myCourse) {
+                    var sectionExists = false;
+                    var sectionIndex = null;
+                    
+                    // Populating catalog
+                    Section.populate(myCourse['sections'], {path: 'catalogs'}, function (err, data) {
+                        
+                        for (var i = 0; i < data.sections.length; i++) {
+                            if (data.sections[i].sectionCode == sectionCode) {
+                                sectionExists = true;
+                                sectionIndex = i;
+                            }
+                        }
+                        
+                        if (!sectionExists) {
+                            //CREATE CATALOG
+                            var newCatalog = new Catalog({
+                                catalogCode: catalogCode,
+                                instructors: catalogInstructors,
+                                meeting: [catalogMeeting]
+                            });
+                            newCatalog.save();
+                            
+                            //CREATE SECTION AND ADD CATALOG TO SECTION
+                            var newSection = new Section({
+                                sectionCode: sectionCode,
+                                term: term,
+                                sectionDirector: sectionDirector,
+                                instructors: sectionInstructors,
+                                sectionMeetings: [],
+                                catalogs: [newCatalog]
+                            });
+                            newSection.save();
+                            
+                            //ADD SECTION TO COURSE
+                            data.sections.push(newSection);
+                            //SAVE
+                            data.save();
+                            
+                        } else {
+                            //CREATE CATALOG
+                            var newCatalog = new Catalog({
+                                catalogCode: catalogCode,
+                                instructors: catalogInstructors,
+                                meeting: [catalogMeeting]
+                            });
+                            newCatalog.save();
+                            
+                            //ADD CATALOG TO SECTION
+                            data.sections[sectionIndex].push(newCatalog);
+                            //SAVE
+                            data.save();
+                        }
+                            
+                        
+                    });
+                
+                //If course is not found    
+                } else {
+                    //CREATE CATALOG
+                    var newCatalog = new Catalog({
+                        catalogCode: catalogCode,
+                        instructors: catalogInstructors,
+                        meeting: [catalogMeeting]
+                    });
+                    newCatalog.save();
+                    
+                    //CREATE SECTION AND ADD CATALOG TO SECTION
+                    var newSection = new Section({
+                        sectionCode: sectionCode,
+                        term: term,
+                        sectionDirector: sectionDirector,
+                        instructors: sectionInstructors,
+                        catalogs: [newCatalog]
+                    });
+                    newSection.save();
+                    
+                    //CREATE COURSE AND ADD SECTION TO COURSE
+                    var newCourse = new Course({
+                        courseCode: courseCode,
+                        facultyCode: facultyCode,
+                        prereqs: prereqs,
+                        exclusions: exclusions,
+                        sections: [newSection],
+                        note: courseNote
+                    });
+                    newCourse.save();
+                    
+                }
+            });            
+        }
+    }
 
 };
