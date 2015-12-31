114d65c125e30d87b3c546f13b6ae14ecb9f22ee
mpirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2015-12-30 21:49:16 -0500

    our latest files

--- model/course.js
+++ model/course.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 //Model for University Courses 
 module.exports = mongoose.model('Course',{
     courseCode: {type:String, required: true}, //courseCode
+    facultyCode: String,
     courseName: String,	//title of the course
     credits: String, //credits for course
     prereqs: [String],  //prerequisites for the course
